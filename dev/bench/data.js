237112be0d4a23d730605934e45deedd73236cfb
github-action-benchmark <github@users.noreply.github.com>
2023-03-06 03:55:22 +0000

    add Threadpool Benchmark (benchmarkjs) benchmark result for 303898cfb190f478874b34cd968ac6f91f46de23

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676583911856,
+  "lastUpdate": 1678074922916,
   "repoUrl": "https://github.com/snuffyDev/nanothreads",
   "entries": {
     "Threadpool Benchmark": [
@@ -52,6 +52,58 @@ window.BENCHMARK_DATA = {
             "range": "±29.64%",
             "unit": "ops/sec",
             "extra": "32 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "72365477+snuffyDev@users.noreply.github.com",
+            "name": "snuffy",
+            "username": "snuffyDev"
+          },
+          "committer": {
+            "email": "72365477+snuffyDev@users.noreply.github.com",
+            "name": "snuffy",
+            "username": "snuffyDev"
+          },
+          "distinct": true,
+          "id": "303898cfb190f478874b34cd968ac6f91f46de23",
+          "message": "fix the benchmarks more",
+          "timestamp": "2023-03-05T22:53:56-05:00",
+          "tree_id": "30aef9572cc7474b48a69b141e8824d66c598d32",
+          "url": "https://github.com/snuffyDev/nanothreads/commit/303898cfb190f478874b34cd968ac6f91f46de23"
+        },
+        "date": 1678074922478,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "nanothreads ([inline] threadpool)",
+            "value": 136719,
+            "range": "±37.78%",
+            "unit": "ops/sec",
+            "extra": "12 samples"
+          },
+          {
+            "name": "nanothreads ([file] threadpool)",
+            "value": 109704,
+            "range": "±54.13%",
+            "unit": "ops/sec",
+            "extra": "15 samples"
+          },
+          {
+            "name": "tinypool",
+            "value": 151194,
+            "range": "±19.12%",
+            "unit": "ops/sec",
+            "extra": "29 samples"
+          },
+          {
+            "name": "threads.js (threadpool)",
+            "value": 1463,
+            "range": "±38.66%",
+            "unit": "ops/sec",
+            "extra": "11 samples"
           }
         ]
       }
